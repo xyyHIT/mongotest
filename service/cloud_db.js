72395f3560eada9e7a77879d4656178604b63342
xuyang <xyy.hit@126.com>
2017-11-27 18:05:35 +0800

    1.fix

--- service/cloud_db.js
+++ service/cloud_db.js
@@ -85,7 +85,7 @@ function connect_cloud_db() {
         
         exports.update_self_column = function (collectionName, cb) {
             var collection = db.collection(collectionName);
-            collection.findAndModify({check: null}, [["_id",1]], {$set:{check:1}}, {new:true},{upsert:true}, function (err, result) {
+            collection.findAndModify({check: null}, [], {$set:{check:1}}, {new:true},{upsert:true}, function (err, result) {
                 if (!err) {
                     cb({record: result});
                 } else {

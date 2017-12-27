67235d115d3b340e8e88c1b295e2b8e13fc4029c
xuyang <xyy.hit@126.com>
2017-12-27 15:48:47 +0800

    1.fix

--- service/connectionDB.js
+++ service/connectionDB.js
@@ -5,5 +5,5 @@ var options = {
 };
 //10.25.203.162:21000,10.26.42.198:21000,10.26.40.75:21000
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://10.25.203.162:20000,10.26.42.198:20000,10.26.40.75:20000/TS_Cloud_DB', options);
+mongoose.connect('mongodb://10.45.177.26:20000,10.45.177.27:20000,10.45.176.29:20000/TS_Cloud_DB', options);
 module.exports = mymongoose;
